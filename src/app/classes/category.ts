19803d8383916209406d0f4244f46c8237b131b3
Faizal Luthfi <zalluth@gmail.com>
2018-10-10 19:12:21 +0700

    refactor: add services relation in category class

--- src/app/classes/category.ts
+++ src/app/classes/category.ts
@@ -1,7 +1,10 @@
+import { Service } from "./service";
+
 export class Category {
   id: number;
   name: string;
   deleted: boolean;
+  services: Service[] = [];
 
   constructor(values: Object = {}) {
     Object.assign(this, values);
