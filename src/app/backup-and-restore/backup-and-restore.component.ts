10f9496e50ceeec92b0d3e19e54e19a66e50ca6c
Faizal Luthfi <zalluth@gmail.com>
2018-10-14 18:23:22 +0700

    refactor: implement backup and restore

--- src/app/backup-and-restore/backup-and-restore.component.ts
+++ src/app/backup-and-restore/backup-and-restore.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./backup-and-restore.component.scss']
 })
 export class BackupAndRestoreComponent implements OnInit {
-
-  constructor() { }
+  private _dialogFilters:any;
 
   ngOnInit() {
+    this._dialogFilters =[
+      {
+        name: `Backup (*.${window['backupExtension']})`,
+        extensions: [window['backupExtension']]
+      }
+    ];
+  }
+
+  backupData() {
+    let now = new Date();
+    let filename = `pos_${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}_${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}`;
+    window['dialog'].showSaveDialog({
+      title: 'Save the backup file as',
+      filters: this._dialogFilters
+    }, filename => {
+      if(filename) {
+        window['ipc'].send('backup', filename);
+      }
+    });
+  }
+
+  restoreData() {
+    window['dialog'].showOpenDialog({
+      title: 'Open the backup file',
+      properties: ['openFile'],
+      filters: this._dialogFilters
+    }, files => {
+      if(files) {
+        window['ipc'].send('restore', files.toString());
+      }
+    });
   }
 
 }
