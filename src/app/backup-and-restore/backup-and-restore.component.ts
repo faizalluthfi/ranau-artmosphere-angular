a712b383195848f2b5f6c76d98a3acfc0de0cfad
Faizal Luthfi <zalluth@gmail.com>
2018-10-14 17:17:50 +0700

    refactor: generate component

---
+++ src/app/backup-and-restore/backup-and-restore.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-backup-and-restore',
+  templateUrl: './backup-and-restore.component.html',
+  styleUrls: ['./backup-and-restore.component.scss']
+})
+export class BackupAndRestoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
