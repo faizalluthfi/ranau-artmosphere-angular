dc432031792d30a1f839dc6cef342c54e2b41c64
Faizal Luthfi <zalluth@gmail.com>
2018-10-26 03:43:23 +0700

    refactor: update classes

--- src/app/classes/report-category.ts
+++ src/app/classes/report-category.ts
@@ -1,2 +1,11 @@
+import { Category } from "./category";
+
 export class ReportCategory {
+  id: number;
+  name: string;
+  categories: Category[] = [];
+
+  constructor(values: Object = {}) {
+    Object.assign(this, values);
+  }
 }
