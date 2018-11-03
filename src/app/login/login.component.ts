c0e00eb4910463f002c3c0712fcca72cb535ecf2
Faizal Luthfi <zalluth@gmail.com>
2018-11-03 18:02:00 +0700

    refactor: implement login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
+import { AuthService } from 'app/services/auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+  form: FormGroup;
+  error: String;
+  subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(
+    formBuilder: FormBuilder,
+    private service: AuthService,
+    private router: Router,
+    private zone: NgZone
+  ) {
+    this.form = formBuilder.group({
+      name: [null, Validators.required],
+      password: [null, Validators.required]
+    });
+  }
 
   ngOnInit() {
+    this.service.logout();
+    this.subscriptions = [
+      this.form.valueChanges.subscribe(() => this.error = null),
+      this.service.error.subscribe(error => this.error = error)
+    ];
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+
+  login() {
+    this.service.login(this.form.value).then(user => {
+      this.zone.run(() => {
+        if (user) {
+          this.router.navigate(['/']);
+        }
+      })
+    });
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { User } from 'app/classes/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  readonly session: Subject<User> = new Subject<User>();
+  readonly error: Subject<String> = new Subject<String>();
+  redirectURL: String;
 
-  constructor() { }
+  login(user: User) {
+    return new window['Users']()
+      .query(qb => {
+        qb
+          .where('name', user.name)
+          .where('password', user.password)
+          .whereNull('deleted')
+      })
+      .fetch()
+      .then(result => {
+        if (result) {
+          let user = result.toJSON();
+          this.session.next(user);
+          sessionStorage.setItem('user', JSON.stringify(user));
+          return user;
+        }
+        this.error.next('Username and password salah.')
+        this.logout();
+        return null;
+      });
+  }
+
+  logout() {
+    sessionStorage.removeItem('user');
+    this.session.next(null);
+  }
+
+  get user(): User {
+    let storage = sessionStorage.getItem('user');
+    let user = storage ? JSON.parse(storage) : null;
+    return user;
+  }
 }
